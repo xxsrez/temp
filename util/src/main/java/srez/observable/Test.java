c94b218dea855fc10b96f56e97c3f2996112178c
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-27 11:15:47 +0300

    Multimodule implemented

--- util/src/main/java/srez/observable/Test.java
+++ util/src/main/java/srez/observable/Test.java
@@ -1,20 +1,8 @@
 package srez.observable;
 
-import com.google.common.base.Stopwatch;
-
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.LongStream;
 
 public class Test {
-    public static void main(String[] args) throws InterruptedException {
-        rx.Observable.interval(1, TimeUnit.SECONDS).subscribe(Test::process);
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        LongStream.range(0, 1000000000).count();
-        System.err.println(stopwatch.elapsed(TimeUnit.MILLISECONDS));
-        Thread.currentThread().join();
-    }
-
     private static void process(Long i) {
         CompletableFuture.runAsync(() -> System.err.println(i));
     }
