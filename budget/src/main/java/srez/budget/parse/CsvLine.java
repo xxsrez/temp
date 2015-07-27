c00ac3b494e0419a432ea9e92e760d8386ad816b
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-27 12:17:41 +0300

    expense lib impl

---
+++ budget/src/main/java/srez/budget/parse/CsvLine.java
@@ -0,0 +1,25 @@
+package srez.budget.parse;
+
+import java.time.LocalDate;
+
+public class CsvLine {
+    String[] tokens;
+
+    public CsvLine(String line) {
+        this.tokens = line.split(";");
+    }
+
+    public String getString(int index) {
+        String result = this.tokens[index];
+        return unwrap(result);
+    }
+
+    public LocalDate getDate(int index) {
+        return LocalDate.parse(getString(index));
+    }
+
+    private String unwrap(String result) {
+        return result.substring(1, result.length() - 1);
+    }
+
+}
