2558e34c0599ed5fe4e353a9660becc0e2ff506f
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-27 11:16:30 +0300

    Multimodule implemented

--- util/src/main/java/srez/observable/Test.java
+++
@@ -1,9 +0,0 @@
-package srez.observable;
-
-import java.util.concurrent.CompletableFuture;
-
-public class Test {
-    private static void process(Long i) {
-        CompletableFuture.runAsync(() -> System.err.println(i));
-    }
-}
