0f2350f60a3a762e5337a1a442e2cf2ee3ee937d
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-28 18:46:23 +0300

    utils

--- util/src/main/java/srez/util/streams/impl/UniqueCollector.java
+++ util/src/main/java/srez/util/streams/impl/UniqueCollector.java
@@ -1,41 +1,59 @@
 package srez.util.streams.impl;
 
+import srez.util.streams.impl.UniqueCollector.Holder;
+
 import java.util.EnumSet;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 
-public class UniqueCollector<T> implements Collector<T, AtomicReference<T>, T> {
+public class UniqueCollector<T> implements Collector<T, Holder<T>, T> {
+    private static final String STREAM_TOO_LONG = "Non unique stream, too much data within";
+    private static final String STREAM_EMPTY = "Non unique stream is empty";
+
     @Override
-    public Supplier<AtomicReference<T>> supplier() {
-        return AtomicReference::new;
+    public Supplier<Holder<T>> supplier() {
+        return Holder::new;
     }
 
     @Override
-    public BiConsumer<AtomicReference<T>, T> accumulator() {
-        return (r, t) -> {
-            if (!r.compareAndSet(null, t)) throw new IllegalStateException();
-        };
+    public BiConsumer<Holder<T>, T> accumulator() {
+        return Holder::set;
     }
 
     @Override
-    public BinaryOperator<AtomicReference<T>> combiner() {
+    public BinaryOperator<Holder<T>> combiner() {
         return (r1, r2) -> {
-            throw new IllegalStateException();
+            throw new IllegalArgumentException(STREAM_TOO_LONG);
         };
     }
 
     @Override
-    public Function<AtomicReference<T>, T> finisher() {
-        return AtomicReference::get;
+    public Function<Holder<T>, T> finisher() {
+        return Holder::get;
     }
 
     @Override
     public Set<Characteristics> characteristics() {
         return EnumSet.noneOf(Characteristics.class);
+    }
+
+    static class Holder<T> {
+        private T t;
+        private boolean isSet;
+
+        public void set(T t) {
+            if (isSet) throw new IllegalArgumentException(STREAM_TOO_LONG);
+            isSet = true;
+            this.t = t;
+        }
+
+        public T get() {
+            if (!isSet) throw new IllegalArgumentException(STREAM_EMPTY);
+            return t;
+        }
     }
 }
