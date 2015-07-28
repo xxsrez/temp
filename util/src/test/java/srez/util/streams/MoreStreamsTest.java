04769394d7cca52e580aa44036e2b6a36c17f4d2
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-28 19:40:41 +0300

    utils

--- util/src/test/java/srez/util/streams/MoreStreamsTest.java
+++ util/src/test/java/srez/util/streams/MoreStreamsTest.java
@@ -1,24 +1,28 @@
 package srez.util.streams;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import static java.util.stream.IntStream.range;
 import static java.util.stream.Stream.iterate;
+import static org.junit.Assert.assertEquals;
 import static srez.util.streams.MoreStreams.*;
 
-public class MoreStreamsTest extends TestCase {
+public class MoreStreamsTest {
+    @Test
     public void testWithIndex() throws Exception {
         withIndex(range(0, 1000).boxed())
                 .forEach(p -> assertEquals(p.getKey(), p.getValue()));
     }
 
+    @Test
     public void testZip() throws Exception {
         zip(range(0, 1000).boxed(), range(0, 1000).boxed(), (i1, i2) -> {
             assertEquals(i1, i2);
-            return 0;
+            return i1;
         });
     }
 
+    @Test
     public void testZipDiffSize() throws Exception {
         assertEquals(100, zip(range(0, 1000).boxed(), range(0, 100).boxed(), (i1, i2) -> {
             assertEquals(i1, i2);
@@ -26,6 +30,15 @@ public class MoreStreamsTest extends TestCase {
         }).count());
     }
 
+    @Test
+    public void testAggregated() throws Exception {
+        assertEquals(10, more(range(0, 4).boxed())
+                .mapAggregated(() -> 0, (Integer i, Integer a) -> i + a)
+                .mapToInt(i -> i)
+                .sum());
+    }
+
+    @Test
     public void testAggregatedTakeWhile() throws Exception {
         assertEquals(14, more(more(iterate(0, i -> i + 1))
                 .mapAggregated(() -> 0, (Integer i, Integer a) -> i + a))
