e9a3b1f02310bcc238be15b5c613a0f568ae2f93
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-29 13:10:27 +0300

    util

--- util/src/main/java/srez/util/Pair.java
+++ util/src/main/java/srez/util/Pair.java
@@ -1,9 +1,19 @@
 package srez.util;
 
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
+
+import static java.util.stream.Collectors.toMap;
 
 public class Pair<K, V> extends SimpleEntry<K, V> {
     public Pair(K key, V value) {
         super(key, value);
     }
+
+    public static <K, V> Collector<Pair<K, V>, ?, Map<K, V>> toMapCollector() {
+        return toMap(Entry::getKey, Entry::getValue);
+    }
+
 }
