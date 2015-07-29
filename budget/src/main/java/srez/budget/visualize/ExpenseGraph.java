9fd5991d844c7e973d55c4b603f4bca4d6d0fb2b
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-29 19:49:26 +0300

    html report

--- budget/src/main/java/srez/budget/visualize/ExpenseGraph.java
+++ budget/src/main/java/srez/budget/visualize/ExpenseGraph.java
@@ -4,18 +4,18 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import srez.budget.analyze.Analyzer;
 
+import java.util.Collection;
+import java.util.Optional;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.summingDouble;
 import static org.jfree.chart.ChartFactory.createXYLineChart;
+import static srez.util.streams.MoreStreams.more;
 
 @Component
 public class ExpenseGraph {
@@ -38,11 +38,14 @@ public class ExpenseGraph {
 
     public XYSeriesCollection dataset() {
         XYSeries series = new XYSeries("expenses");
-        Stream.of(analyzer.getExpenses())
-                .collect(groupingBy(e -> e.getPostingDate().toEpochDay(), summingDouble(k -> k.getMoney().getMoney())))
-                .entrySet().stream()
-                .map(e -> new XYDataItem(e.getKey().doubleValue(), e.getValue().doubleValue()))
-                .forEach(series::add);
+        more(analyzer.getGrouped().stream())
+                .withIndex()
+                .forEach(p -> series.add(
+                        p.getKey().doubleValue(),
+                        Optional.ofNullable(p.getValue()).map(Collection::stream).orElse(Stream.empty())
+                                .mapToDouble(e -> e.getMoney().getMoney())
+                                .sum()
+                ));
         return new XYSeriesCollection(series);
     }
 
