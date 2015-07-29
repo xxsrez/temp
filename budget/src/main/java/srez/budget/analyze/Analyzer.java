a2dc444abc613d8bff819fb4704df741daba9861
Andrey Myasnikov <xxsrez@gmail.com>
2015-07-29 19:43:30 +0300

    html report

--- budget/src/main/java/srez/budget/analyze/Analyzer.java
+++ budget/src/main/java/srez/budget/analyze/Analyzer.java
@@ -6,12 +6,19 @@ import srez.budget.parse.Expense;
 import srez.budget.parse.ExpenseLoader;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.Map;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 import static java.util.stream.Stream.of;
 
 @Component
 public class Analyzer {
     private Expense[] expenses;
+    private List<List<Expense>> groupped;
 
     @Autowired
     ExpenseLoader expenseLoader;
@@ -21,9 +28,22 @@ public class Analyzer {
         expenses = of(expenseLoader.getExpenses())
                 .filter(e -> !e.getDescription().contains("Own funds transfer"))
                 .toArray(Expense[]::new);
+
+        Map<Long, List<Expense>> byDate = of(expenseLoader.getExpenses())
+                .collect(groupingBy(e -> e.getPostingDate().toEpochDay()));
+        LongSummaryStatistics statistics = byDate.keySet().stream()
+                .mapToLong(i -> i)
+                .summaryStatistics();
+        groupped = range(0, (int) (statistics.getMax() - statistics.getMin() + 1))
+                .mapToObj(byDate::get)
+                .collect(toList());
     }
 
     public Expense[] getExpenses() {
         return expenses;
+    }
+
+    public List<List<Expense>> getGroupped() {
+        return groupped;
     }
 }
