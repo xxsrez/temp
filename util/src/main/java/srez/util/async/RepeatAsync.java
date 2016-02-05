49686c1c055d9be45ee49adb0b86665ab0c5fb27
Andrey Myasnikov <xxsrez@gmail.com>
2016-02-06 00:26:36 +0300

    async

--- util/src/main/java/srez/util/async/RepeatAsync.java
+++ util/src/main/java/srez/util/async/RepeatAsync.java
@@ -1,14 +1,19 @@
 package srez.util.async;
 
+import static java.util.stream.IntStream.range;
+import static srez.util.async.NamedDaemonFactory.newFixedThreadPool;
+
 public class RepeatAsync extends AbstractAsync {
     private final int threadCount;
 
     public RepeatAsync(int threadCount) {
         this.threadCount = threadCount;
+        executor(newFixedThreadPool(threadCount, this, "worker"));
     }
 
     @Override
     public Cancellation doExec(Runnable runnable) {
+        range(0, threadCount).forEach(i -> runnable.run());
         return new Cancellation(() -> {
         });
     }
