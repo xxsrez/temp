d875bd66c869a8ba4e3eaaa1903c2d1d778de079
Andrey Myasnikov <xxsrez@gmail.com>
2016-02-06 12:35:07 +0300

    size util test

--- util/src/test/java/srez/util/format/SizeUtilTest.java
+++ util/src/test/java/srez/util/format/SizeUtilTest.java
@@ -7,67 +7,67 @@ import static srez.util.format.SizeUtil.bytesToString;
 
 public class SizeUtilTest {
     @Test
-    public void testT1() throws Exception {
+    public void test25Gb() throws Exception {
         smartAssert((long) ((2 << 30) * 2.5), "2.5Gb");
     }
 
     @Test
-    public void testT2() throws Exception {
+    public void test2Gb() throws Exception {
         smartAssert(2 << 31, "2Gb");
     }
 
     @Test
-    public void testT3() throws Exception {
+    public void test1024Mb() throws Exception {
         smartAssert(2 << 30, "1024Mb");
     }
 
     @Test
-    public void testT4() throws Exception {
+    public void test2Mb() throws Exception {
         smartAssert(2 << 21, "2Mb");
     }
 
     @Test
-    public void testT5() throws Exception {
+    public void test1024Kb() throws Exception {
         smartAssert(2 << 20, "1024Kb");
     }
 
     @Test
-    public void testT6() throws Exception {
+    public void test2Kb() throws Exception {
         smartAssert(2 << 11, "2Kb");
     }
 
     @Test
-    public void testT7() throws Exception {
+    public void test1024b() throws Exception {
         smartAssert(2 << 10, "1024bytes");
     }
 
     @Test
-    public void testT8() throws Exception {
+    public void test2b() throws Exception {
         smartAssert(2, "2bytes");
     }
 
     @Test
-    public void testT9() throws Exception {
+    public void test1b() throws Exception {
         smartAssert(1, "1byte");
     }
 
     @Test
-    public void testT10() throws Exception {
+    public void test0b() throws Exception {
         smartAssert(0, "0bytes");
     }
 
     @Test
-    public void testT11() throws Exception {
+    public void testM1b() throws Exception {
         smartAssert(-1, "-1bytes");
     }
 
     @Test
-    public void testT12() throws Exception {
+    public void testM2Gb() throws Exception {
         smartAssert(-2 << 31, "-2Gb");
     }
 
     @Test
-    public void testT13() throws Exception {
+    public void testM1024M() throws Exception {
         smartAssert(-2 << 30, "-1024Mb");
     }
 
