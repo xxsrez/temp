293d7d5a5ba089ce5c9adf2850a90540082fcdbf
Andrey Myasnikov <xxsrez@gmail.com>
2016-02-06 12:09:45 +0300

    size util

--- util/src/main/java/srez/util/format/SizeUtil.java
+++ util/src/main/java/srez/util/format/SizeUtil.java
@@ -1,8 +1,28 @@
 package srez.util.format;
 
+import java.text.DecimalFormat;
+
 public class SizeUtil {
+    private static final ThreadLocal<DecimalFormat> format = ThreadLocal.withInitial(() -> new DecimalFormat("#.##"));
+
+    private static final String[] MODS = {"Gb", "Mb", "Kb"};
+
     public static String bytesToString(long bytes) {
-        //TODO
-        return bytes + "";
+        double bytesDouble = bytes;
+        int div = 2 << 30;
+        int index = 0;
+
+        String postfix = bytes + "";
+
+        while (index <= MODS.length) {
+            double divided = bytesDouble / div;
+            if (divided > 2) {
+                String mod = MODS[index];
+                return format.get().format(divided) + mod + " " + postfix;
+            }
+            div >>= 10;
+            index++;
+        }
+        return postfix;
     }
 }
